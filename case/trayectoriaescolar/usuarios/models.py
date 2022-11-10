ba8e5e694794365ba71a2e73a2b841e35e393f55
JeMaJuPa <114031821+JeMaJuPa@users.noreply.github.com>
2022-11-10 00:15:58 -0600

    Ya casi registra xd

---
+++ case/trayectoriaescolar/usuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Administradores(User):
+    nombre = models.CharField(max_length=100)
+    correo = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.nombre
