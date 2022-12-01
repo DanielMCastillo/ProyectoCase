b78fbec4ce258bde4b3199d211df8dedacd7d06a
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2022-11-30 18:53:33 -0600

    Modificaciones CASE SSTE
    Nuevo diseño, urls hasta registro de admin

--- trayectoriaescolar/usuarios/urls.py
+++ trayectoriaescolar/usuarios/urls.py
@@ -6,11 +6,11 @@ from django.contrib.auth.views import LogoutView
 app_name = 'usuarios'
 
 urlpatterns = [
-
+    path('login/', views.LoginView.as_view(), name='login'),
     path('', views.BienvenidaView.as_view(), name='bienvenida'),
     path('administradores', views.lista_admins, name='administradores'),
     path('salir', LogoutView.as_view(), name='logout'),
-    path('entrar', views.LoginView.as_view(), name='login'),
-    path('registrar', views.RegistrarAdmin.as_view(), name='registrar'),
+    path('registro_de_administrador', views.RegistrarAdmin.as_view(), name='registro_de_administrador'),
+    
 
 ]
--- trayectoriaescolar/usuarios/views.py
+++ trayectoriaescolar/usuarios/views.py
@@ -2,12 +2,13 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from .forms import UserForm
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 
 class BienvenidaView(LoginRequiredMixin, TemplateView):
     success_url = reverse_lazy('bienvenida')
@@ -15,7 +16,7 @@ class BienvenidaView(LoginRequiredMixin, TemplateView):
 
 
 class LoginView(LoginView):
-    success_url = reverse_lazy('bienvenida')
+    success_url = reverse_lazy('index_case.html')
     template_name = 'login.html'
     form_class = AuthenticationForm
 
@@ -23,11 +24,19 @@ class LoginView(LoginView):
 class RegistrarAdmin(CreateView):
     model = User
     form_class = UserForm
+    template_name = 'registro_de_administrador.html'
     success_url = reverse_lazy('usuarios:login')
     success_message = "%(username)s se registró de manera exitosa"
-
 
 
 def lista_admins(request):
     administradores = User.objects.all()
     return render(request, 'administradores.html', {'administradores': administradores})
+
+
+
+# Función para cerrar sesión.
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('myLogin')
